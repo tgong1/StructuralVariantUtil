53e69f931471ba859453fb83aaf6704274aa89e4
tgong1 <tingting.gong@hotmail.com>
2022-11-23 10:57:54 +0800

    update SV integration remove duplicated output

--- R/SV_integration_filtration.R
+++ R/SV_integration_filtration.R
@@ -177,6 +177,7 @@ SV_integration <- function(vcf_files, SVCaller_names, sampleID = "sample_1", bkp
     }
     union_ID <- caller1_bedpe[, (ncol(caller1_bedpe)-length(SVCaller_names)+1):ncol(caller1_bedpe)]
     integrated_bedpe <- caller1_bedpe[!duplicated(union_ID),]
+    union_ID <- integrated_bedpe[, (ncol(integrated_bedpe)-length(SVCaller_names)+1):ncol(integrated_bedpe)]
     for(i in c(1: length(SVCaller_names))){
       integrated_bedpe <- integrated_bedpe[!(duplicated(union_ID[,i]) & (!is.na(union_ID[,i]))),]
     }
